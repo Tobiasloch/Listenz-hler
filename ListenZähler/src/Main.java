a91be428cdd921e7d5322733893f6d255654a915
Tobias Loch <Tobias.Loch@online.de>
2016-11-27 11:38:10 +0100

    renamed the accept button to Start

--- ListenZähler/src/Main.java
+++ ListenZähler/src/Main.java
@@ -67,7 +67,7 @@ public class Main {
 		path.setBounds(10, 200, 250, 30);
 		jf.add(path);
 		
-		acceptButton = new JButton("gaartssa");
+		acceptButton = new JButton("Start");
 		acceptButton.addActionListener(new AListener());
 		acceptButton.setBounds(280, 235, 100, 30);
 		jf.add(acceptButton);
